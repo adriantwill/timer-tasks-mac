364d2e45261d6273e06450f13ba77208923c3011
adriantwill <142526523+adriantwill@users.noreply.github.com>
2026-02-01 11:38:54 -0800

    Initial Commit

---
+++ timer-tasks-mac/timer_tasks_macApp.swift
@@ -0,0 +1,17 @@
+//
+//  timer_tasks_macApp.swift
+//  timer-tasks-mac
+//
+//  Created by Adrian Will on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct timer_tasks_macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
