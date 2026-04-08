02a216775f366c9d2b499d9d4201b16430bb185d
adriantwill <142526523+adriantwill@users.noreply.github.com>
2026-04-08 18:01:28 -0400

    fix task progress not saving

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use clap::{Parser, Subcommand};
 use serde::{Deserialize, Serialize};
 use std::fs;
-use std::io;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Serialize, Deserialize)]
@@ -26,41 +25,34 @@ struct Cli {
 #[derive(Subcommand, Debug)]
 enum Commands {
     Add { name: String },
-    Start { id: i32 },
+    Start { task_name: String },
     Stop,
     Status,
 }
 fn main() {
     let cli = Cli::parse();
 
-    let mut input = String::new();
     let text = fs::read_to_string("tasks.json").expect("failed to read tasks.json");
     let mut app_state: AppState = serde_json::from_str(&text).unwrap();
-    let mut tasks = app_state.tasks;
     // tasks.push(Task {
     //     id: 1,
     //     name: String::from("test1"),
     //     time: 0,
     // });
-    input.clear();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("you failed to read line");
-    let parts: Vec<&str> = input.trim().split_whitespace().collect();
     match cli.command {
         Commands::Add { name } => {
-            tasks.push(Task {
+            app_state.tasks.push(Task {
                 id: 0,
-                name: String::from("test"),
+                name: name,
                 time: 0,
             });
         }
-        Commands::Start { id } => {
-            let Some(task_name) = parts.get(1) else {
-                println!("Enter a task name");
-                return;
-            };
-            let Some(task) = tasks.iter_mut().find(|task| task.name == *task_name) else {
+        Commands::Start { task_name } => {
+            let Some(task) = app_state
+                .tasks
+                .iter_mut()
+                .find(|task| task.name == *task_name)
+            else {
                 println!("Task not found");
                 return;
             };
@@ -74,11 +66,13 @@ fn main() {
                         .unwrap()
                         .as_secs(),
                 );
+                let serialized = serde_json::to_string(&app_state).unwrap();
+                fs::write("tasks.json", serialized).expect("failed to write tasks.json");
             }
         }
         Commands::Stop => {
             if let (Some(task_id), Some(s)) = (app_state.started_task_id, app_state.started_at) {
-                let Some(task) = tasks.iter_mut().find(|task| task.id == task_id) else {
+                let Some(task) = app_state.tasks.iter_mut().find(|task| task.id == task_id) else {
                     println!("Task not found");
                     return;
                 };
@@ -89,14 +83,14 @@ fn main() {
                     - s;
                 println!("{} took {} seconds", task.name, elapsed);
                 task.time += elapsed;
-                let serialized = serde_json::to_string(&tasks).unwrap();
-                fs::write("tasks.json", serialized).expect("failed to write tasks.json");
                 app_state.started_task_id = None;
                 app_state.started_at = None;
+                let serialized = serde_json::to_string(&app_state).unwrap();
+                fs::write("tasks.json", serialized).expect("failed to write app_state.tasks.json");
             }
         }
         Commands::Status => {
-            for task in &tasks {
+            for task in &app_state.tasks {
                 let (progress, task_time) = if Some(task.id) == app_state.started_task_id
                     && let Some(s) = app_state.started_at
                 {
@@ -114,9 +108,6 @@ fn main() {
                 };
                 println!("{}: {} seconds ({})", task.name, task_time, progress);
             }
-        }
-        _ => {
-            println!("Invalid command, enter stop or start");
         }
     }
 }
